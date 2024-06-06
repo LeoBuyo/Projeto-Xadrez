84987a4ca9d002069bcd29ae75d26746bd124541
LeoBuyo <leoaguri@gmail.com>
2024-06-06 16:43:11 -0300

    Created Project

---
+++ xadrez-console/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using xadrez_console.Board;
+namespace xadrez_console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Position p = new Position(3, 4);
+
+            Console.WriteLine("Position: " + p);
+
+            Console.ReadLine();    
+        }
+    }
+}
